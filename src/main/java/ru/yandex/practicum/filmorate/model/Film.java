f57b1fb49fd1dd04ca5fe6c6e333da5c3e9e3a6d
alina zhavrid <alinazavrid@gmail.com>
2026-05-11 15:55:24 +0300

    java-sprint11-hw

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
@@ -26,7 +27,7 @@ public class Film {
     @Positive(message = "Продолжительность должна быть положительным числом")
     private Integer duration;
 
-    private Set<Long> likes;
+    private Set<Long> likes=new HashSet<>();
 
     @AssertTrue(message = "дата релиза не может быть раньше 28 декабря 1895 года")
     public boolean isReleaseDateValid() {
